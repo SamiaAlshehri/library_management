1891f86bf69fcfea879a0d220129474576f2d343
frappe <frappe@ubuntu>
2022-04-06 18:52:36 +0000

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Library managment System
+
+ Library Management System in which the Librarian can log in and manage Articles and Memberships Library Management System in which the Librarian can log in and manage Articles and Memberships
+
+#### License
+
+MIT
---
+++ library_management/config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Library managment System",
+			"color": "browen",
+			"icon": "octicon octicon-file-directory",
+			"type": "module",
+			"label": _("Library managment System")
+		}
+	]
